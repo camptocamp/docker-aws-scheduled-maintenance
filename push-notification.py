22fa9203b004529ad98940de9d965d9aa1e69a23
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2017-02-21 10:30:44 +0100

    added some debug, and better event management

--- push-notification.py
+++ push-notification.py
@@ -9,27 +9,39 @@ if len(sys.argv) == 2:
             profile_name = sys.argv[1],
             region_name  = 'eu-west-1',
             )
+    print('Using profile %s'%sys.argv[1])
 else:
     session = boto3.session.Session(
             aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
             region_name=os.environ['AWS_REGION_NAME'],
             )
+    print('Using AWS key %s'%os.environ['AWS_ACCESS_KEY_ID'])
+
 ec2_client = session.client('ec2')
 registry = CollectorRegistry()
 
 scheduled_downtime = Counter('aws_scheduled_downtime', 'AWS scheduled downtimes', registry=registry)
+scheduled_retire   = Counter('aws_scheduled_retire', 'AWS scheduled retirements', registry=registry)
 impaired_systems   = Counter('aws_impaired_systems', 'AWS running systems with bad state', registry=registry)
 impaired_instances = Counter('aws_impaired_instances', 'AWS running instances with bad state', registry=registry)
 
 for status in ec2_client.describe_instance_status(IncludeAllInstances=True)['InstanceStatuses']:
     if 'Events' in status:
-        scheduled_downtime.inc()
+        print('Found: scheduled maintenance')
+        if any('stop' in s['Code'] for s in status['Events']) or any('retire' in s['Code'] for s in status['Events']):
+            print('  Is a retirement')
+            scheduled_retire.inc()
+        else:
+            print('  Is a reboot')
+            scheduled_downtime.inc()
     if status['InstanceState']['Name'] == 'running':
         if status['SystemStatus']['Status'] == 'impaired':
             impaired_systems.inc()
+            print('Found: impaired system')
         if status['InstanceStatus']['Status'] == 'impaired':
             impaired_instances.inc()
+            print('Found: impaired instance')
 
 push_to_gateway('pushgateway.metrics:9091', job='AWS_States', registry=registry)
 
