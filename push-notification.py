696a29335f5fcda53bf0199c5f2759909774fcfa
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2017-02-21 10:48:00 +0100

    better exception management, better display of logs

--- push-notification.py
+++ push-notification.py
@@ -3,20 +3,21 @@ from prometheus_client import CollectorRegistry, Counter, push_to_gateway
 import boto3
 import os
 import sys
+import urllib.error
 
 if len(sys.argv) == 2:
     session = boto3.session.Session(
             profile_name = sys.argv[1],
             region_name  = 'eu-west-1',
             )
-    print('Using profile %s'%sys.argv[1])
+    print('[I] Using profile %s'%sys.argv[1])
 else:
     session = boto3.session.Session(
             aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
             region_name=os.environ['AWS_REGION_NAME'],
             )
-    print('Using AWS key %s'%os.environ['AWS_ACCESS_KEY_ID'])
+    print('[I] Using AWS key %s'%os.environ['AWS_ACCESS_KEY_ID'])
 
 ec2_client = session.client('ec2')
 registry = CollectorRegistry()
@@ -28,21 +29,24 @@ impaired_instances = Counter('aws_impaired_instances', 'AWS running instances wi
 
 for status in ec2_client.describe_instance_status(IncludeAllInstances=True)['InstanceStatuses']:
     if 'Events' in status:
-        print('Found: scheduled maintenance')
+        print('[I] Found: scheduled maintenance')
         if any('stop' in s['Code'] for s in status['Events']) or any('retire' in s['Code'] for s in status['Events']):
-            print('  Is a retirement')
+            print('[W]  Is a retirement')
             scheduled_retire.inc()
         else:
-            print('  Is a reboot')
+            print('[I]  Is a reboot')
             scheduled_downtime.inc()
     if status['InstanceState']['Name'] == 'running':
         if status['SystemStatus']['Status'] == 'impaired':
             impaired_systems.inc()
-            print('Found: impaired system')
+            print('[I] Found: impaired system')
         if status['InstanceStatus']['Status'] == 'impaired':
             impaired_instances.inc()
-            print('Found: impaired instance')
+            print('[I] Found: impaired instance')
 
-push_to_gateway('pushgateway.metrics:9091', job='AWS_States', registry=registry)
+try:
+    push_to_gateway('pushgateway.metrics:9091', job='AWS_States', registry=registry)
+except urllib.error.URLError:
+    print('[C] Unable to push to Prometheus')
 
 sys.exit(0)
