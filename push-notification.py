cefc8f5e164c70c8b2c6e004cc7387dcb5d9473f
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2017-02-22 12:19:04 +0100

    Two major changes in this commit, please read bellow
    1- Better management for sessions and errors
    The script now gracefuly fails and try to send data to prometheus
    regarding its issues.
    
    2- Use labels for prometheus data instead of multiple indexes
    Instead of having 4-5 counters only for ec2 instances events, there is
    now only one, and we're using labels in order to allow a distinction.
    This also allows to push ec2 instance_id in addition for better
    troubleshooting and actions.

--- push-notification.py
+++ push-notification.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
 from prometheus_client import CollectorRegistry, Counter, push_to_gateway
 import boto3
+import botocore.exceptions
 import os
 import sys
 import urllib.error
 
+registry = CollectorRegistry()
+
 if len(sys.argv) == 2:
+    print('[I] Using profile %s'%sys.argv[1])
     session = boto3.session.Session(
             profile_name = sys.argv[1],
             region_name  = 'eu-west-1',
             )
-    print('[I] Using profile %s'%sys.argv[1])
 else:
-    session = boto3.session.Session(
-            aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-            aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-            region_name=os.environ['AWS_REGION_NAME'],
-            )
-    print('[I] Using AWS key %s'%os.environ['AWS_ACCESS_KEY_ID'])
+    print('[I] Using either environment var or instance profile')
+    session = boto3.session.Session()
+
+
+if not session.region_name:
+    print('[C] Critical error in script: no region_name set')
+    error = Counter('aws_event_checker_fails', 'Errors with script', registry=registry)
+    error.inc()
+    try:
+        print('[I] Pushing to prometheus')
+        push_to_gateway('pushgateway.metrics:9091', job='AWS_States', registry=registry)
+    except urllib.error.URLError:
+        print('[C] Unable to push to Prometheus')
+    sys.exit(1)
+
 
 ec2_client = session.client('ec2')
-registry = CollectorRegistry()
 
-scheduled_downtime = Counter('aws_scheduled_downtime', 'AWS scheduled downtimes', registry=registry)
-scheduled_retire   = Counter('aws_scheduled_retire', 'AWS scheduled retirements', registry=registry)
-impaired_systems   = Counter('aws_impaired_systems', 'AWS running systems with bad state', registry=registry)
-impaired_instances = Counter('aws_impaired_instances', 'AWS running instances with bad state', registry=registry)
+ec2_events = Counter('aws_ec2_events', 'Events on AWS EC2 instances', ['instance_id', 'event_code'], registry=registry)
 
 events = ec2_client.describe_instance_status(IncludeAllInstances=True)['InstanceStatuses']
 print('[I] %i instances'%len(events))
@@ -33,17 +41,19 @@ for status in events:
     if 'Events' in status:
         print('[I] Found: scheduled maintenance')
         if any('stop' in s['Code'] for s in status['Events']) or any('retire' in s['Code'] for s in status['Events']):
-            print('[W]  Is a retirement')
-            scheduled_retire.inc()
+            print('[W]  Is a retirement (%s)'%status['InstanceId'])
+            ec2_events.labels(status['InstanceId'], 'termination').inc()
+        elif any('reboot' in s['Code'] for s in status['Events']):
+            print('[I]  Is a reboot (%s)'%status['InstanceId'])
+            ec2_events.labels(status['InstanceId'], 'reboot').inc()
         else:
-            print('[I]  Is a reboot')
-            scheduled_downtime.inc()
+            print(status['Events'])
     if status['InstanceState']['Name'] == 'running':
         if status['SystemStatus']['Status'] == 'impaired':
-            impaired_systems.inc()
+            ec2_events.labels(status['InstanceId'], 'system_impaired').inc()
             print('[I] Found: impaired system')
         if status['InstanceStatus']['Status'] == 'impaired':
-            impaired_instances.inc()
+            ec2_events.labels(status['InstanceId'], 'instance_impaired').inc()
             print('[I] Found: impaired instance')
 
 try:
@@ -51,5 +61,6 @@ try:
     push_to_gateway('pushgateway.metrics:9091', job='AWS_States', registry=registry)
 except urllib.error.URLError:
     print('[C] Unable to push to Prometheus')
+
 
 sys.exit(0)
